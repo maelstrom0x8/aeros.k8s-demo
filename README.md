b2a2b9bceca5531eaf9c03e3fc818731d55c47ed
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-08-07 06:37:03 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # demo
+
+Demo Spring application used for learning Kubernetes.
+
+- Make changes and commit them
+- Add tag
+    `git tag vX.X.X`
+- Push tag to remote (CI will be triggered for only changes with tags)
+- `docker pull maelstrom0x8/demo:vX.X.X-arch` to get image from docker hub
+
+A sample K8s manifest file
+
+```yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: demo-service
+  labels:
+    name: demo
+spec:
+  selector:
+    matchLabels:
+      name: demo
+  replicas: 4
+  template:
+    metadata:
+      name: aeros-demo
+      labels:
+        name: demo
+    spec:
+      containers:
+        - name: demo-native
+          image: maelstrom0x8/demo:v0.0.4-x86_64
+          ports:
+          - containerPort: 9090
+          resources:
+            limits:
+              cpu: 100m
+              memory: 500Mi
+            
+
+
+--- 
+
+apiVersion: v1
+kind: Service
+metadata:
+  name: dm-svc
+  labels:
+    name: demo
+spec:
+  selector:
+    name: demo
+  ports:
+  - port: 8988
+    protocol: TCP
+    targetPort: 9090
+  type: ClusterIP
+
+```
--- src/main/java/org/aeros/demo/Application.java
+++ src/main/java/org/aeros/demo/Application.java
@@ -1,9 +1,14 @@
 package org.aeros.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class Application {
@@ -15,14 +20,22 @@ public class Application {
 
 	@RestController
 	public class DemoController {
+		private final Logger LOG = LoggerFactory.getLogger(DemoController.class);
 
 		@GetMapping("/demo/hello")
-		public String hello() {
-			var os = System.getProperty("os.name");
+		public String hello() throws IOException {
+			var os = execReadToString("hostname").trim();
 			var arch = System.getProperty("os.arch");
 			var username = System.getProperty("user.name");
 
-			return "You've hit " + username + "@" + os + "-" + arch;
+			LOG.info("{}@{}-{} was hit", username, os, arch);
+			return "Hello!";
+		}
+	}
+
+	public static String execReadToString(String execCommand) throws IOException {
+		try (Scanner s = new Scanner(Runtime.getRuntime().exec(execCommand).getInputStream()).useDelimiter("\\A")) {
+			return s.hasNext() ? s.next() : "";
 		}
 	}
 
